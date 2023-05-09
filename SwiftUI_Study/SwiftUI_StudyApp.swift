c73013660f99d4967b5905f2cf24843099544f25
JeongAYoo <jeonga038@naver.com>
2023-05-10 01:31:33 +0900

    Initial Commit

---
+++ SwiftUI_Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI_Study
+//
+//  Created by Jade Yoo on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
